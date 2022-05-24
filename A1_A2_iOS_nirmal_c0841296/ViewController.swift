b2bf93ac5422e00f973efd8ec26de9b1db9d5bf3
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-05-24 18:05:45 -0400

    project initialized

---
+++ A1_A2_iOS_nirmal_c0841296/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_nirmal_c0841296
+//
+//  Created by nirmal on 2022-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
